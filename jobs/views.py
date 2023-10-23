1face0fdff9996917652292739eff0959c82ca14
rafah <rafah.hawa@yahoo.fr>
2023-10-23 19:11:39 +0200

    add views, templates, urls, static part 1

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Job, Company, Category
 
+class JobList(generic.ListView):
+    model=Job
 # Create your views here.
+
+class JobDetail(generic.DetailView):
+    model=Job
